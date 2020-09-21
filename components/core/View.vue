c14d85c929e9b99478f8489903c48b4cdd0137a9
patrickchow <patrickchow688@gmail.com>
2020-09-22 05:04:54 +0800

    Initial Commit - 22Sept2020_5am - Initial commit of mytca base template

---
+++ components/core/View.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-main class="grey lighten-3">
+    <nuxt />
+
+    <core-footer />
+  </v-main>
+</template>
+
+<script>
+import Footer from '@/components/core/Footer'
+
+export default {
+  // name: 'DashboardCoreView',
+
+  components: {
+    CoreFooter: Footer
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <core-app-bar />
+    <core-drawer />
+    <core-view />
+    <core-settings />
+  </v-app>
+</template>
+
+<script>
+import AppBar from '@/components/core/AppBar'
+import Drawer from '@/components/core/Drawer'
+import View from '@/components/core/View'
+import Settings from '@/components/core/Settings'
+
+export default {
+  components: {
+    CoreAppBar: AppBar,
+    CoreDrawer: Drawer,
+    CoreView: View,
+    CoreSettings: Settings
+  },
+  // props: {
+  //   source: String
+  // },
+  data: () => ({
+    // drawer: null,
+    expandOnHover: false
+  })
+}
+</script>
